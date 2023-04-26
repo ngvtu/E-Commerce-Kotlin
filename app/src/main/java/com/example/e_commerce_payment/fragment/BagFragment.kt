2b2c96c5198464483406a6f8c411c0566007d3ae
Nguyễn Văn Tú <nvtu1612@gmail.com>
2023-04-26 23:58:12 +0700

    create 5 fragment

---
+++ app/src/main/java/com/example/e_commerce_payment/fragment/BagFragment.kt
@@ -0,0 +1,20 @@
+package com.example.e_commerce_payment.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.e_commerce_payment.R
+
+class BagFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bag, container, false)
+    }
+
+}
